5dfaac29a32ba1ff17a1bcb40a3352a511f4fd99
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-04-26 16:29:04 +0900

    Simplify yuyv loop

--- src/from_yuyv.zig
+++ src/from_yuyv.zig
@@ -11,21 +11,19 @@ pub fn yuyvToI420(yuyv_data: []const u8, i420_data: []u8, width: u32, height: u3
     const v_plane = i420_data[(width * height + width * height / 4)..];
 
     var uv_idx: usize = 0;
-
     var y: usize = 0;
-    while (y < height) : (y += 2) {
+    while (y < height) : (y += 1) {
         var x: usize = 0;
         while (x < width) : (x += 2) {
             const yuyv_idx = (y * width + x) * 2;
 
             y_plane[y * width + x] = yuyv_data[yuyv_idx];
             y_plane[y * width + x + 1] = yuyv_data[yuyv_idx + 2];
-            y_plane[(y + 1) * width + x] = yuyv_data[yuyv_idx + width * 2];
-            y_plane[(y + 1) * width + x + 1] = yuyv_data[yuyv_idx + width * 2 + 2];
-
-            u_plane[uv_idx] = yuyv_data[yuyv_idx + 1];
-            v_plane[uv_idx] = yuyv_data[yuyv_idx + 3];
-            uv_idx += 1;
+            if (y & 1 == 0) {
+                u_plane[uv_idx] = yuyv_data[yuyv_idx + 1];
+                v_plane[uv_idx] = yuyv_data[yuyv_idx + 3];
+                uv_idx += 1;
+            }
         }
     }
 }
